86eb58afdf1bc843d25555f17458cd9024ab833c
wastech <fataiwasiu2@gmail.com>
2022-05-17 00:03:09 +0100

    add client and configure client folder

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  
   const conn = await mongoose.connect(process.env.MONGO_LOCAL_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
